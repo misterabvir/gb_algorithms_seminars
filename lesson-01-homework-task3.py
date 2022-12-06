b6ba03b4383943124b03035de5b090080579873f
misterabvir <abvir@yandex.ru>
2022-12-06 18:32:26 +0300

    add python code

---
+++ lesson-01-homework-task3.py
@@ -0,0 +1,10 @@
+index = 0
+sum = 0
+count = 4
+number = 0
+while index < count:
+    number = int(input("Type number: "))
+    sum = sum + number
+    index = index + 1
+adv = sum / count
+print(adv)
---
+++ lesson-01-homework-task6.py
@@ -0,0 +1,7 @@
+n = 5
+result = 1
+
+while n >= 2:
+    result = result * n
+    n = n - 1
+print(result)
